dac2e876fcbba1e4c6d3d783dd947ae89a8111d6
Liam Stabeno <liamstabeno@gmail.com>
2017-02-27 19:28:05 -0800

    create initial files

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+//Back End
+function Player(name, hp, atk)  {
+  this.name = name;
+  this.hp = hp;
+  this.atk= atk;
+}
+var players = {name: "The fighters of the octogon", fighters: [player1, player2, player3]};
+
+
+
+//Front End
+$(function(){
+  $("//buttonname").submit(function()  {
+  var Player(name) =
+
+
+});
+
+
+
+});
