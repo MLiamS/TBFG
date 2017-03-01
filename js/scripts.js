76bb934fba23a7934ce83535e9e6dde933e948a7
liam stabeno <liamstabeno@gmail.com>
2017-02-28 17:04:17 -0800

    add canvas

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,43 @@
-//Back End
-function Player(name, hp, atk)  {
+// Back End
+function startGame() {
+    myGameArea.start();
+}
+//
+// var myGameArea = {
+//     canvas : document.createElement("canvas"),
+//     start : function() {
+//         this.canvas.width = 480;
+//         this.canvas.height = 270;
+//         this.context = this.canvas.getContext("2d");
+//         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+//     }
+// }
+
+
+
+
+function Player(name, hp, atk){
   this.name = name;
   this.hp = hp;
   this.atk= atk;
 }
-var players = {name: "The fighters of the octogon", fighters: [player1, player2, player3]};
+
+
+
+var hp = 100;
+var atk= 50;
 
 
 
 //Front End
 $(function(){
-  $("//buttonname").submit(function()  {
-  var Player(name) =
-
-
+  $("form#input-form").submit(function(event) {
+  event.preventDefault();
+  var inputName = $("input#nameInput").val();
+  var newPlayer =  new Player(inputName, hp, atk);
+  $("#play1").append("<li>" + newPlayer.name + "</li>");
+  $("#play1").append("<li>" + newPlayer.hp + "</li>"+ "</li>");
+  $("#play1").append("<li>" + newPlayer.atk + "</li>");
 });
 
 
